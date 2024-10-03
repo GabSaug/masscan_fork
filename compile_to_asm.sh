790f45406b2e1031a9a72d8ea4eadb0b31f18657
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-10-03 16:03:34 +0200

    [bug] Made the script launching folder agnostic

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -2,7 +2,9 @@
 # $2 output path
 opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
 
-if ! cc  -I ./src/ -Wall $opt -S -masm=intel "$1" -o "$2"; then
+script_path=$(realpath $(dirname $0))
+
+if ! cc  -I $script_path/src/ -Wall $opt -S -masm=intel "$1" -o "$2"; then
 	echo "error compile to asm"
 	exit 1
 fi
